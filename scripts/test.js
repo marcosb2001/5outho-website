9055d27b6ae54b4761e7271f8eeeb5001547ca9b
Marcos Benitez <MarcossBenitezm@outlook.com>
2022-11-21 15:12:53 -0300

    112122 1

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,8 @@
-const obras = ['1', '2', '3', '4']
+const obras = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 console.log (...obras)
+console.log("hi, testing js integration, i forgot how to do this")
+
+var randomItem = obras[Math.floor(Math.random()*obras.length)]
+
+console.log(randomItem)
