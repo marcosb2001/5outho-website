9b2dc0fe3a815545a2fd8a178978756b0ebcd075
Marcos Benitez <MarcossBenitezm@outlook.com>
2022-11-21 17:46:48 -0300

    112122 2, random image on index

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,22 @@
-const obras = ['1', '2', '3', '4', '5', '6', '7', '8']
+const obras = [
+    {id: 1, img: './media/draw.jpeg', txt: "untitled, 2022, 80x60cm"},
+    {id: 2, img: './media/em.png', txt: "mii"},
+    {id: 3, img: './media/fr.png', txt: "frog"},
+    {id: 4, img: './media/blo.png', txt: "idk"},
+    {id: 5, img: './media/mew.png', txt: "mewtwo"},
+    {id: 6, img: './media/place.jpg', txt: "a place"},
+]
 
 console.log (...obras)
 console.log("hi, testing js integration, i forgot how to do this")
-
 var randomItem = obras[Math.floor(Math.random()*obras.length)]
+console.log(randomItem.img)
 
-console.log(randomItem)
+const indexgallery = document.getElementById('drawing')
+
+indexgallery.innerHTML = "<img src="+randomItem.img+">  <p>"+randomItem.txt+"</p>"
+
+
+
+
+
