48e6c9391dcde1909ac64011ee84ca0db872dd02
Marcos Benitez <MarcossBenitezm@outlook.com>
2022-11-22 22:48:01 -0300

    112222 1

--- scripts/test.js
+++ scripts/test.js
@@ -1,20 +1,19 @@
 const obras = [
-    {id: 1, img: './media/draw.jpeg', txt: "untitled, 2022, 80x60cm"},
-    {id: 2, img: './media/em.png', txt: "mii"},
-    {id: 3, img: './media/fr.png', txt: "frog"},
-    {id: 4, img: './media/blo.png', txt: "idk"},
-    {id: 5, img: './media/mew.png', txt: "mewtwo"},
-    {id: 6, img: './media/place.jpg', txt: "a place"},
+    {id: 1, img: './media/art/draw.jpeg', txt: "untitled, 2022, 80x60cm"},
+    {id: 2, img: './media/art/walking.gif', txt: "walking, 2022, animation"},
+    {id: 3, img: './media/art/huhu.png', txt: "gaguuusad, 2022, digital"},
+    {id: 4, img: './media/art/weatcher4.png', txt: "creepy, 2022, digital"},
+    {id: 5, img: './media/art/hutnwood2.png', txt: "fruitful hunt, 2022, digital"},
+    {id: 6, img: './media/art/place.jpg', txt: "a place"},
 ]
 
-console.log (...obras)
-console.log("hi, testing js integration, i forgot how to do this")
+console.log("hello, im  testing js integration, i kinda forgot how to do this out of not practicing often enough so...pls don't make fun of me if my code sucks..")
 var randomItem = obras[Math.floor(Math.random()*obras.length)]
 console.log(randomItem.img)
 
 const indexgallery = document.getElementById('drawing')
 
-indexgallery.innerHTML = "<img src="+randomItem.img+">  <p>"+randomItem.txt+"</p>"
+indexgallery.innerHTML = "<img src="+randomItem.img+"> <p>"+randomItem.txt+"</p>"
 
 
 
